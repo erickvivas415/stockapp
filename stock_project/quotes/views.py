5bc5417b85a6dca575f807f2b47bdd1fde2ddbcc
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-06-14 06:56:25 -0700

    Ticker search active

--- stock_project/quotes/views.py
+++ stock_project/quotes/views.py
@@ -5,15 +5,19 @@ def home(request):
     import requests
     import json
 
-    # API Key: pk_bedfd3f917bf4d758890c03617cc4d01
-    APITOKEN = "pk_bedfd3f917bf4d758890c03617cc4d01"
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_bedfd3f917bf4d758890c03617cc4d01")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        # API Key: pk_bedfd3f917bf4d758890c03617cc4d01
+        APITOKEN = "pk_bedfd3f917bf4d758890c03617cc4d01"
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_bedfd3f917bf4d758890c03617cc4d01")
 
-    api = json.loads(api_request.content)
-    #try:
-    #    api = json.loads(api_request.symbol)
-    #except Exception as e:
-    #    api = "Error..."
+        #api = json.loads(api_request.content)
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a ticker symbol above..."})
 
     return render(request, 'home.html', {'api': api})
 
