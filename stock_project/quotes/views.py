6f7b324cc1a555620878785fe9aa2fc798db316b
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-06-13 19:51:17 -0700

    Stock info

--- stock_project/quotes/views.py
+++ stock_project/quotes/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    # API Key: pk_bedfd3f917bf4d758890c03617cc4d01
+    APITOKEN = "pk_bedfd3f917bf4d758890c03617cc4d01"
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_bedfd3f917bf4d758890c03617cc4d01")
+
+    api = json.loads(api_request.content)
+    #try:
+    #    api = json.loads(api_request.symbol)
+    #except Exception as e:
+    #    api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
